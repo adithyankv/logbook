bc532fb9039e3243ca0e60eff5d4d49fa21d8d41
adithyankv <adithyankv@protonmail.com>
2022-01-29 20:45:14 +0530

    Adds settings menu

--- logbook/headerbar.py
+++ logbook/headerbar.py
@@ -17,17 +17,19 @@ class HeaderBar(Gtk.HeaderBar):
         info_button = Gtk.Button.new_from_icon_name(
             "dialog-information", Gtk.IconSize.LARGE_TOOLBAR
         )
-        settings_button = Gtk.Button.new_from_icon_name(
+        settings_button = Gtk.MenuButton()
+        settings_button.props.image = Gtk.Image.new_from_icon_name(
             "open-menu", Gtk.IconSize.LARGE_TOOLBAR
         )
+        settings_button.props.popover = self.create_settings_menu()
         info_button.connect("clicked", self.show_info_dialog)
 
         self.pack_end(settings_button)
         self.pack_end(info_button)
 
-    def show_info_dialog(self, _) -> None:
+    def show_info_dialog(self, *args) -> None:
         info_text = [
-            "Logbook is a viewer for systemd logs.",
+            "Logbook is a viewer for systemd logs.\n",
             "You can read some or all of the logs depending on your permissions.",
             "These permissions may be set by your distro or your administrator.",
         ]
@@ -39,3 +41,21 @@ class HeaderBar(Gtk.HeaderBar):
         )
         info_dialog.run()
         info_dialog.destroy()
+
+    def create_settings_menu(self) -> Gtk.Popover:
+        menu_box = Gtk.Box()
+
+        reverse_item = Granite.SwitchModelButton()
+        reverse_item.props.active = True
+        reverse_item.props.text = "Reverse order"
+        reverse_item.props.description = "Show latest logs first"
+
+        menu_box.add(reverse_item)
+        menu_box.props.margin_top = 3
+        menu_box.props.margin_bottom = 3
+        menu_box.show_all()
+
+        settings_menu = Gtk.Popover()
+        settings_menu.add(menu_box)
+
+        return settings_menu
