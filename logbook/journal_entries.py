3346645cb2209840b7d08132689b4542afea6843
adithyankv <adithyankv@protonmail.com>
2022-01-29 16:22:21 +0530

    Implements reading entries from journal

---
+++ logbook/journal_entries.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from systemd import journal
+
+
+class JournalEntries:
+    def __init__(self, reversed: bool = True) -> None:
+        self.journal_reader = journal.Reader()
+        self.reversed = reversed
+        if self.reversed:
+            self.journal_reader.seek_tail()
+        self.journal_reader.this_boot()
+
+    def __iter__(self) -> JournalEntries:
+        return self
+
+    def __next__(self) -> dict:
+        if self.reversed:
+            entry = self.journal_reader.get_previous()
+        else:
+            entry = self.journal_reader.get_next()
+
+        if entry:
+            return entry
+        else:
+            raise StopIteration
+
+
+journal_entries = JournalEntries()
+for entry in journal_entries:
+    print(entry["MESSAGE"])
