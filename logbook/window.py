28ca06f4c78894d2e85331f2b0d3632e0247d8d8
adithyankv <adithyankv@protonmail.com>
2022-01-30 21:46:06 +0530

    Saving settings to gsettings

--- logbook/window.py
+++ logbook/window.py
@@ -1,12 +1,11 @@
 import gi
 
-from logbook.entry_item import EntryItem
 from logbook.entry_list import EntryList
 from logbook.headerbar import HeaderBar
 
 gi.require_version("Gtk", "3.0")
 
-from gi.repository import GObject, Gtk  # noqa: E402
+from gi.repository import Gio, Gtk  # noqa: E402
 
 
 class LogbookWindow(Gtk.ApplicationWindow):
@@ -14,6 +13,10 @@ class LogbookWindow(Gtk.ApplicationWindow):
         super().__init__()
         self.set_application(application)
         self.create_layout()
+
+        self.load_config_from_schema()
+
+        self.connect("delete_event", self.save_config_to_schema)
 
     def create_layout(self) -> None:
         self.props.default_width = 600
@@ -26,3 +29,23 @@ class LogbookWindow(Gtk.ApplicationWindow):
         scrolled_window.add(entry_list)
         self.set_titlebar(headerbar)
         self.add(scrolled_window)
+
+    def load_config_from_schema(self) -> None:
+        self.settings = Gio.Settings(schema_id="com.github.adithyankv.logbook")
+        x = self.settings.get_int("position-x")
+        y = self.settings.get_int("position-y")
+        self.move(x, y)
+
+        width = self.settings.get_int("width")
+        height = self.settings.get_int("height")
+        self.resize(width, height)
+
+    def save_config_to_schema(self, *args) -> None:
+        width, height = self.get_size()
+        x, y = self.get_position()
+
+        self.settings.set_int("position-x", x)
+        self.settings.set_int("position-y", y)
+
+        self.settings.set_int("width", width)
+        self.settings.set_int("height", height)
